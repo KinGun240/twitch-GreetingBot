04f46ec304fdea6d62bdf2f0444198a4a5ae4b6e
KinGun <kingunsq@gmail.com>
2022-06-18 03:16:39 +0900

    update:v1.2.0への対応

--- GreetingBot.py
+++ GreetingBot.py
@@ -17,16 +17,18 @@ import unicodedata
 Debug = False
 
 # バージョン
-ver = '1.1.3'
-userExpFile = "userExpList.csv"
+ver = '1.2.0'
+
+# 固定値
+CommentLogFile = "commentLog.csv"
+UserExpFile = "userExpList.csv"
+ErrorLogFile = 'errorLog.csv'
 
 # 各種初期設定 #####################################
 # bot用コンフィグの読み込み
 try:
     sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
     configGreeting = importlib.import_module('config')
-    # configGreeting.Twitch_Channel = configGreeting.Twitch_Channel.lower()
-    # configGreeting.Trans_Username = configGreeting.Trans_Username.lower()
     print("Read config.py")
     # remove "#" mark ------
     if configGreeting.Twitch_Channel.startswith('#'):
@@ -38,7 +40,7 @@ try:
         configGreeting.Bot_OAUTH = configGreeting.Bot_OAUTH[6:]
 except Exception as e:
     print(e)
-    print('Please make [config.py] and put it with Greetingbot')
+    print('Please make [config.py] and put it with GreetingBot')
     input()  # stop for error!!
 
 # botの初期化
@@ -61,39 +63,39 @@ try:
     print("Read param_greetingBot.py")
 except Exception as e:
     print(e)
-    print('Please make [param_greetingBot.py] and put it with Greetingbot')
+    print('Please make [param_greetingBot.py] and put it with GreetingBot')
     input()  # stop for error!!
 
 
 # 内部変数の設定 ----------
 # ユーザー経験値リストの読み込み
 try:
-    UserExpList = pd.read_csv(f"./data/{userExpFile}")
-    print(f"Read {userExpFile}")
+    UserExpList = pd.read_csv(f"./data/{UserExpFile}")
+    print(f"Read {UserExpFile}")
 except Exception as e:
     print(e)
-    print(f'Please check [{userExpFile}] and put it with data folder')
+    print(f'Please check [{UserExpFile}] and put it with data folder')
     input()
 if Debug:
     print(UserExpList)
 
-# ユーザー経験値リストに経験値テーブルに基づいたレベルを設定
+# ユーザーメッセージ数リストにコメント数テーブルに基づいたレベルを設定
 try:
     UserExpList['Level'] = 0
     for index, row in UserExpList.iterrows():
         for stepIndex, stepNum in enumerate(greetingParam.ExpTable):
             if row.MessageCount < stepNum:
-                UserExpList.at[index, 'Level'] = stepIndex
+                UserExpList.at[index, 'Level'] = int(stepIndex)
                 break
 except Exception as e:
     print(e)
-    print('Please check [param_greetingBot.py] and put it with Greetingbot')
+    print('Please check [param_greetingBot.py] and put it with GreetingBot')
     input()
 if Debug:
     print(UserExpList)
 
 # 初見ユーザーリストの初期化
-FirstUserList = ['']
+FirstUserList = []
 # FirstUserList = [str.lower() for str in FirstUserList]
 
 
@@ -110,68 +112,47 @@ async def event_ready():
 
     # 書き込み開始のファイル出力
     if greetingParam.IsSaveCommentsFile:
-        with open('comments.csv', 'a') as commentsFile:
+        with open(CommentLogFile, 'a') as commentsFile:
             writer = csv.writer(commentsFile)
             writer.writerow(['#Start Write Comments'])
             commentsFile.close()
+        print(f'{CommentLogFile}にコメント保存を開始します')
 
 
 # メッセージ受信時処理 ----------
 @bot.event
 async def event_message(ctx):
-    msg = ctx.content
-    time = pd.Timestamp(ctx.timestamp, unit='s', tz='UTC')
-    time = time.tz_convert('Asia/Tokyo')
+    onComment = False
+    onLevelup = False
+    onFirstMsg = False
     user = ctx.author.name.lower()
     name = ctx.author.display_name
+    msg = ctx.content
+    time = (pd.Timestamp(ctx.timestamp, unit='s', tz='UTC')
+            ).tz_convert('Asia/Tokyo')
     # isMod = ctx.author.is_mod
     # is_Sub = ctx.author.is_subscriber
-    # if len(ctx.author.badges) > 0:
-    #     badges = ctx.author.badges['broadcaster']
-    # else:
-    #     badges = None
-    strLen = len(msg)
-    multiLen = count_text(msg)
 
     if Debug:
-        print(f'echo: {ctx.echo}, {ctx.content}')
-    # メッセージがコマンドの場合は抜ける
+        print(f'echo: {ctx.echo}, {msg}')
+    # メッセージがコマンドまたはbotの投稿の場合は処理終了
     await bot.handle_commands(ctx)
-    if ctx.content.startswith('!'):
-        return
-    # メッセージがbotの投稿の場合は抜ける
     if not Debug:
-        if ctx.echo or user == bot.nick:
+        if ctx.content.startswith('!') or ctx.echo or user == bot.nick:
             return
+    print(f'\nTIME:{time}\nUSER:{user}\nMSG:{msg}')
 
-    # コメント処理 ----------
-    # コメント用の効果音を鳴らす
-    try:
-        if greetingParam.IsPlaySoundComment:
-            playsound(f"./sound/{greetingParam.CommentSound}", True)
-    except Exception as e:
-        print('sound error: Please check [config.py] and sound folder...')
-        if Debug:
-            print(e.args)
+    # コメント反応処理 ----------
+    # コメント反応のフラグ処理
+    if greetingParam.IsPlaySoundComment:
+        onComment = True
 
-    # タイムスタンプ、ユーザー、コメントのファイル出力
-    try:
-        if greetingParam.IsSaveCommentsFile:
-            with open('comments.csv', 'a') as commentsFile:
-                writer = csv.writer(commentsFile)
-                writer.writerow([time, user, msg])
-                commentsFile.close()
-    except Exception as e:
-        print('file error: [comments.csv] can not save...')
-        if Debug:
-            print(e.args)
-
-    # レベルアップ処理 ----------
+    # 集計コメント反応処理 ----------
     global UserExpList
     key = 'UserName'
     value = 'MessageCount'
-    # ユーザーがユーザー経験値リストに存在する場合は経験値を１追加
-    # 存在しない場合はユーザー経験値リストに追加
+    # ユーザーがユーザーメッセージ数リストに存在する場合はMessageCountを１追加
+    # 存在しない場合はユーザーメッセージ数リストに追加
     count = 1
     try:
         row = UserExpList.query(f"{key} in ['{user}']")
@@ -181,82 +162,96 @@ async def event_message(ctx):
         else:
             newRow = pd.DataFrame([[user, count]], columns=[key, value])
             UserExpList = UserExpList.append(newRow, ignore_index=True)
-        print(UserExpList)
     except Exception as e:
         print('error: UserExpList not read...')
         if Debug:
             print(e.args)
 
-    # ユーザー経験値リストを保存しなおす
+    # ユーザーのメッセージ数に応じてレベルアップさせる
     try:
-        UserExpList.to_csv(f"data/{userExpFile}", index=False)
+        oldLevel = UserExpList.loc[UserExpList[key] == user, 'Level'].item()
+        newLevel = 0
+        row = UserExpList.query(f"{key} in ['{user}']")
+        for stepIndex, stepNum in enumerate(greetingParam.ExpTable):
+            if row[value].item() < stepNum:
+                newLevel = stepIndex
+                UserExpList.loc[UserExpList[key] ==
+                                user, 'Level'] = int(newLevel)
+                break
     except Exception as e:
-        print(f'file error: [{userExpFile}] can not save...')
+        print('error: UserExpList not read...')
+        if Debug:
+            print(e.args)
+    print(UserExpList)
+
+    # レベルアップのフラグ処理
+    if newLevel > oldLevel:
+        onLevelup = True
+
+    # 初回コメント挨拶処理 ----------
+    global FirstUserList
+    # ユーザーが初見ユーザーリストに存在しない場合は初見ユーザーリストへ追加
+    # かつ初見コメント挨拶のフラグ処理
+    if user not in FirstUserList:
+        FirstUserList.append(user)
+        onFirstMsg = True
+    print(f'FirstUserList:{FirstUserList}')
+
+    # 音声・メッセージ処理 ----------
+    # 各フラグ状態に応じてメッセージ出力
+    try:
+        if onFirstMsg and greetingParam.IsGreetingComment:
+            out_text = f'{name} ' + greetingParam.GreetingMessage
+            await ctx.channel.send("/me " + out_text)
+            print(f'BotMessage:{out_text}')
+
+        if onLevelup and greetingParam.IsLevelupComment:
+            out_text = f'{name} ' + greetingParam.LevelupMessage1 + \
+                f'{newLevel}' + greetingParam.LevelupMessage2
+            await ctx.channel.send("/me " + out_text)
+            print(f'BotMessage:{out_text}')
+    except Exception as e:
+        print('message send error')
         if Debug:
             print(e.args)
 
-    # 経験値に応じてレベルアップさせる
-    oldLevel = UserExpList.loc[UserExpList[key] == user, 'Level'].item()
-    newLevel = 0
-    row = UserExpList.query(f"{key} in ['{user}']")
-    for stepIndex, stepNum in enumerate(greetingParam.ExpTable):
-        if row[value].item() < stepNum:
-            newLevel = stepIndex
-            UserExpList.loc[UserExpList[key] == user, 'Level'] = newLevel
-            break
-    if Debug:
-        print(UserExpList)
-
-    # レベルアップが発生していたらコメントを出力する・効果音を鳴らす
-    if newLevel > oldLevel:
-        if greetingParam.IsLevelupComment:
-            if strLen != multiLen:
-                out_text = f"{name} さんのレベルが{newLevel}に上がりました！"
-            else:
-                out_text = f"{name}-san's level has risen to {newLevel}!"
-            await ctx.channel.send("/me " + out_text)
-        if greetingParam.IsPlaySoundLevelup:
-            try:
-                playsound(f"./sound/{greetingParam.LevelupSound}", True)
-            except Exception as e:
-                print('sound error: Please check [config.py] and sound folder')
-                if Debug:
-                    print(e.args)
-
-    # 挨拶処理 ----------
-    global FirstUserList
-    # ユーザーが初見ユーザーリストに存在する場合は処理終了
-    # いない場合は初見ユーザーリストへ追加
-    print('USER:{}'.format(user))
-    if user in FirstUserList:
-        return
-    FirstUserList.append(user)
-
-    # チャット欄へ初見コメント出力
-    if greetingParam.IsGreetingComment:
-        if strLen != multiLen:
-            out_text = f'{name} さん、ようこそ！'
-        else:
-            out_text = f'{name}-san, Hello!!'
-        await ctx.channel.send("/me " + out_text)
-
-    # 初見挨拶用の効果音を鳴らす
+    # 各フラグ状態に応じて音声出力
     try:
-        if greetingParam.IsPlaySoundGreeting:
+        if onFirstMsg and greetingParam.IsPlaySoundGreeting:
             playsound(f"./sound/{greetingParam.GreetingSound}", True)
+            print(f'BotSound:{greetingParam.GreetingSound}')
+        elif onComment:
+            playsound(f"./sound/{greetingParam.CommentSound}", True)
+            print(f'BotSound:{greetingParam.CommentSound}')
+
+        if onLevelup and greetingParam.IsPlaySoundLevelup:
+            playsound(f"./sound/{greetingParam.LevelupSound}", True)
+            print(f'BotSound:{greetingParam.LevelupSound}')
     except Exception as e:
         print('sound error: Please check [config.py] and sound folder...')
         if Debug:
             print(e.args)
 
-    # コンソールへの表示
-    print(out_text)
+    # その他処理 ----------
+    # コメントログ保存処理:タイムスタンプ、ユーザー名、コメント内容
+    try:
+        if greetingParam.IsSaveCommentsFile:
+            with open(CommentLogFile, 'a') as commentsFile:
+                writer = csv.writer(commentsFile)
+                writer.writerow([time, user, msg])
+                commentsFile.close()
+    except Exception as e:
+        print(f'file error: [{CommentLogFile}] can not save...')
+        if Debug:
+            print(e.args)
 
-
-# ハローコマンド時処理 ----------
-@bot.command()
-async def hello(ctx):
-    await ctx.send(f'{ctx.author.name}さん こんにちは～ KonCha')
+    # ユーザーメッセージ数リストを保存しなおす
+    try:
+        UserExpList.to_csv(f"data/{UserExpFile}", index=False)
+    except Exception as e:
+        print(f'file error: [{UserExpFile}] can not save...')
+        if Debug:
+            print(e.args)
 
 
 #####################################
--- param_greetingBot.py
+++ param_greetingBot.py
@@ -1,19 +1,26 @@
 # パラメーター項目 ###########################
-# コメントファイルの保存の有無
+# コメントログ保存の有無
 IsSaveCommentsFile = False
-# コメント時の音声の有無
+
+# コメント反応の音声の有無
 IsPlaySoundComment = True
-# コメント時の音声ファイル、WAVかMP3形式
+# コメント反応の音声ファイル、WAVかMP3形式
 CommentSound = 'metal03.wav'
 
 # レベルアップコメントの有無
 IsLevelupComment = True
+# レベルアップコメントのBotメッセージ
+# 例）LevelupMessage1 = 'さんは'
+# 　　LevelupMessage2 = 'にレベルアップ！'
+# 　　[Bot名]:[コメントしたユーザー] さんは[新しい数値]にレベルアップ！
+LevelupMessage1 = 'さんのレベルが'
+LevelupMessage2 = 'に上がりました！'
 # レベルアップ時の音声の有無
 IsPlaySoundLevelup = True
 # レベルアップ時の音声ファイル、WAVかMP3形式
 LevelupSound = 'tm2_power001.wav'
-# 経験値テーブル
-#  デフォルト(Level n - 必要な経験値)
+# コメント数テーブル
+#  デフォルト(Level n - 必要なコメント数)
 #  Level 1 - 2
 #  Level 2 - 4
 #  Level 3 - 8
@@ -27,9 +34,13 @@ LevelupSound = 'tm2_power001.wav'
 ExpTable = [(2**1), (2**2), (2**3), (2**4), (2**5), (2**6), (2**7), (2**8),
             (2**9), 999]
 
-# 挨拶コメントの有無
+# 初回コメント挨拶の有無
 IsGreetingComment = True
-# 挨拶コメント時の音声の有無
+# 初回コメント挨拶のBotメッセージ
+# 例）GreetingMessage = 'さん、ゆっくりしていってください。'
+# 　　[Bot名]:[コメントしたユーザー] さん、ゆっくりしていってください。
+GreetingMessage = 'さん、ようこそ！'
+# 初回コメント挨拶の音声の有無
 IsPlaySoundGreeting = True
-# 挨拶コメント時の音声ファイル、WAVかMP3形式
+# 初回コメント挨拶の音声ファイル、WAVかMP3形式
 GreetingSound = 'tm2_chime002.wav'
